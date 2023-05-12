33adfd5242dd80c2b3155dcc94451f32c7c49a25
haidohong <dohaimail@gmail.com>
2023-05-12 21:29:46 +0700

    topic/class-members

--- Lab 2/DigitalVideoDisc.java
+++ Lab 2/DigitalVideoDisc.java
@@ -1,27 +1,12 @@
 
 public class DigitalVideoDisc {
+    private static int nbDigitalVideoDiscs = 0;
+    private int id;
     private String title;
     private String category;
     private String director;
     private int length;
     private float cost;
-    
-    public DigitalVideoDisc(String title) {
-        this.title = title;
-    }
-    
-    public DigitalVideoDisc(String title, String category, float cost) {
-        this.title = title;
-        this.category = category;
-        this.cost = cost;
-    }
-    
-    public DigitalVideoDisc(String title, String category, String director, float cost) {
-        this.title = title;
-        this.category = category;
-        this.director = director;
-        this.cost = cost;
-    }
     
     public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
         this.title = title;
@@ -29,7 +14,26 @@ public class DigitalVideoDisc {
         this.director = director;
         this.length = length;
         this.cost = cost;
+        ++nbDigitalVideoDiscs;
+        this.id = nbDigitalVideoDiscs;
     }
+    public DigitalVideoDisc(String title, String category, float cost) {
+        this(title, category, null, 0, cost);
+
+    } 
+
+    public DigitalVideoDisc(String title) {
+        this(title, null, null, 0, 0f);
+    }
+    
+    
+    
+    public DigitalVideoDisc(String title, String category, String director, float cost) {
+        this(title, category, director, 0, cost);
+
+    }
+    
+    
 
     public String getTitle() {
         return title;
--- README.md
+++ README.md
@@ -2,3 +2,5 @@ This file will descript the process of completing the tasks in the Lab 03:
 1. Create a branch "refactor/apply-release-flow"
 2. Create branch "topic/method-overloading", adding some function using method overloading.
 3. Create branch "topic/passing-parameter", using Testing class to confirm that Java only pass by value. Create a new swap function to make sure that the new swap function really do what it is desingated to.
+4. In Report.
+5. Create branch "topic/class-members", modify constructor, adding static member.
