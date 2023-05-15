49a892a9bcbb69384fa8959ce2b0326af2098e11
haidohong <dohaimail@gmail.com>
2023-05-15 15:40:03 +0700

    Add files that are lost in the refactor progress

--- AIMS/src/hust/soict/globalict/test/store/StoreTest.java
+++ AIMS/src/hust/soict/globalict/test/store/StoreTest.java
@@ -1,6 +1,7 @@
-package hust.soict.globalict.aims.store;
+package hust.soict.globalict.test.store;
 
 import hust.soict.globalict.aims.disc.DigitalVideoDisc;
+import hust.soict.globalict.aims.store.Store;
 
 public class StoreTest {
     public static void main(String[] args) {
